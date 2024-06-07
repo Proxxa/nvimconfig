09bfd2d7568700988509b8cba06ef5370a996324
Proxxa <yorip625@gmail.com>
2024-06-07 08:04:17 -0400

    Configure telescope extra

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,24 @@
+local map = require("util.map")
+
 return {
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             require("telescope").setup {
+                defaults = {
+                    sorting_strategy = "ascending",
+                    layout_config = {
+                        prompt_position = "top"
+                    },
+                },
                 extensions = {
                     file_browser = {
                         display_stat = {
                             date = true,
                             size = true,
                         },
+                        grouped = true,
                         hide_parent_dir = true,
                         hijack_netrw = true,
                     },
@@ -23,6 +32,7 @@ return {
         dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
         config = function()
             require("telescope").load_extension("file_browser")
+            map.n("<leader>e", "<cmd>:Telescope file_browser<cr>")
         end,
     },
     {
@@ -30,7 +40,25 @@ return {
         dependencies = { 'nvim-telescope/telescope.nvim' },
         config = function()
             require("telescope").load_extension("ui-select")
-            require("util.map").n("<leader>e", "<cmd>:Telescope file_browser<cr>")
         end,
-   }
+    },
+    {
+        'nvim-telescope/telescope-project.nvim',
+        dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-telescope/telescope-file-browser.nvim' },
+        config = function()
+            require("telescope").load_extension("project")
+            map.n("<leader>p", "<cmd>:Telescope project<cr>")
+        end
+    },
+    {
+        'nvim-telescope/telescope-media-files.nvim',
+        dependencies = {
+            'nvim-lua/popup.nvim',
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+        },
+        config = function() 
+            require("telescope").load_extension("media_files")
+        end
+    },
  }
