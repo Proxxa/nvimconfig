aaf39de3d46140ac451594db98b592fa58b1fb93
Proxxa <yorip625@gmail.com>
2024-06-08 19:07:44 -0400

    Remove seemingly broken Telescope extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -50,15 +50,4 @@ return {
             map.n("<leader>p", "<cmd>:Telescope project<cr>")
         end
     },
-    {
-        'nvim-telescope/telescope-media-files.nvim',
-        dependencies = {
-            'nvim-lua/popup.nvim',
-            'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope.nvim',
-        },
-        config = function() 
-            require("telescope").load_extension("media_files")
-        end
-    },
  }
