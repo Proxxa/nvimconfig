63739215e5913c37251e2ba39de7e17f76bdad84
Proxxa <yorip625@gmail.com>
2024-10-01 19:40:45 -0400

    Improve LSP and completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,21 @@
-return { 
+return {
     'neovim/nvim-lspconfig',
-    -- config = function()
-    --
-    --     require"lspconfig".svelte.setup{}
-    -- end,
+    config = function()
+        local lspc = require"lspconfig"
+        lspc.clangd.setup{
+            init_options = {
+                fallbackFlags = { "--std=c++20" },
+            },
+        }
+
+        lspc.rust_analyzer.setup {
+            settings = {
+                ["rust_analyzer"] = {
+                    cargo = {
+                        allFeatures = true,
+                    },
+                },
+            },
+        }
+    end,
 }
